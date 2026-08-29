6e5c10b0aa97b02f3a7f3192a08dba7fe82fef2f
fnaz3108 <fnaz3108@gmail.com>
2026-08-29 20:53:40 +0500

    Load static footer rock signature

--- assets/hero-world.js
+++ assets/hero-world.js
@@ -1,3 +1,5 @@
+import './footer-rock.js';
+
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
